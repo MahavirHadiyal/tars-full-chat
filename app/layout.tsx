0833a0e36904af5dd7b044627eba38f9a2279db5
Hadiyal Mahavir <mghadiyal0@gmail.com>
2026-02-21 23:20:08 +0530

    fix layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,16 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import "./globals.css";
+import Providers from "./providers";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <ClerkProvider>
-      <ConvexProvider client={convex}>
-        <html lang="en">
-          <body>{children}</body>
-        </html>
-      </ConvexProvider>
-    </ClerkProvider>
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
   );
 }
